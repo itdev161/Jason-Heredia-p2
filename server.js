afa649539b3a935c31a5b3b57afcf7a296023c3a
Jason Heredia <heredijr@gmatc.matc.edu>
2020-12-10 00:07:45 -0600

    User Registration

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
 
 // Initialize express App
 const app = express();
@@ -7,10 +8,40 @@ const app = express();
 // Connect database
 connectDatabase();
 
+// Configure Middleware
+app.use(express.json({ extended: false }));
+
 // API endpoints
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
 app.get('/', (req, res) => 
     res.send('http get request sent to root api endpoint')
 );
+
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+app.post('/api/users',
+[
+    check('gameID', 'Please enter your gameID')
+        .not()
+        .isEmpty(),
+    check('email', 'Please enter a valid email')
+        .isEmail(),
+    check('password', 'Please enter a password with 6 or more characters')
+        .isLength({ min: 6 })
+],
+(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    } else {
+        return res.send(req.body);
+    }
+});
 
 // Conection Listener
 app.listen(3000, () => console.log(`Express server running on port 3000`));
